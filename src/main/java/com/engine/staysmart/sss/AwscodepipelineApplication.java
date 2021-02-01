95bf492a78ec99a30f4f172dabb0f1d7d21785c3
sallubhai <abidhsk123@gmail.com>
2021-02-01 19:21:06 +0530

    intinal commit

---
+++ src/main/java/com/engine/staysmart/sss/AwscodepipelineApplication.java
@@ -0,0 +1,13 @@
+package com.engine.staysmart.sss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscodepipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwscodepipelineApplication.class, args);
+	}
+
+}
